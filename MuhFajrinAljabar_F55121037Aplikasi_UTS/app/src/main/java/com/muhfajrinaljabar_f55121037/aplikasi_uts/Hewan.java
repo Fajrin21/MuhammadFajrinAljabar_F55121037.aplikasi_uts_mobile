3d86cb49100e6d440a6a004b2407550a3f1e3526
fajrin <muhfajrin709@gmail.com>
2022-12-01 18:42:32 +0800

    utsmobile

---
+++ MuhFajrinAljabar_F55121037Aplikasi_UTS/app/src/main/java/com/muhfajrinaljabar_f55121037/aplikasi_uts/Hewan.java
@@ -0,0 +1,31 @@
+package com.muhfajrinaljabar_f55121037.aplikasi_uts;
+
+public class Hewan {
+    private int photo;
+    private String name;
+    private String description;
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
